0fc2b8d5346e7e64fcd2538f4b00631e720d5b77
Mujhtech <mujeeb.muhideen@gmail.com>
2022-10-04 22:28:29 +0100

    complete step one

---
+++ lib/splash/view/splash_page.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:devfest/home/home.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
